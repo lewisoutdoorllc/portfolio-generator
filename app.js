9982be315c4584d3c84f08656e1d6a4831e61abe
Cody <74194707+lewisoutdoorllc@users.noreply.github.com>
2021-02-15 11:48:05 -0500

    Create app.js

---
+++ app.js
@@ -0,0 +1,28 @@
+/*
+var commandLineArgs = process.argv;
+console.log(commandLineArgs);
+*/
+
+/*
+var profileDataArgs = process.argv.slice(2, process.argv.length);
+console.log(profileDataArgs);
+*/
+
+/*
+const profileDataArgs = process.argv.slice(2, process.argv.length);
+console.log(profileDataArgs);
+*/
+/*
+const printProfileData = (profileDataArr) => {
+    console.log(profileDataArr);
+  };
+
+  printProfileData(profileDataArgs);
+*/
+
+  // Notice the lack of parentheses around the `profileDataArr` parameter?
+const printProfileData = profileDataArr => {
+    for (let i = 0; i < profileDataArr.length; i++) {
+      console.log(profileDataArr[i]);
+    }
+  };
