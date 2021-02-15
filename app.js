1fbf4d288a21a6cb9bea9515276f785469a392b5
Cody <74194707+lewisoutdoorllc@users.noreply.github.com>
2021-02-15 18:57:51 -0500

    adjusted changes

--- app.js
+++ app.js
@@ -131,12 +131,20 @@ promptUser()
   .then(portfolioData => {
     const pageHTML = generatePage(portfolioData);
 
+    fs.writeFile('./index.html', pageHTML, err => {
+      if (err) throw new Error(err);
+
+      console.log('Page created! Check out index.html in this directory to see it!');
+    });
+  });
+
+
     // fs.writeFile('./index.html', pageHTML, err => {
     //   if (err) throw new Error(err);
 
     //   console.log('Page created! Check out index.html in this directory to see it!');
     // });
-  });
+
 // const fs = require('fs');
 // const generatePage = require('./src/page-template.js');
 
